7143d242b5992c1eef7b490700399700e4602bd8
trevorfarthing <tfarthing@zagmail.gonzaga.edu>
2020-05-22 14:13:47 -0700

    Initial site revamp

---
+++ slide-menu.js
@@ -0,0 +1,9 @@
+/* Open the sidenav */
+function openNav() {
+  document.getElementById("sideNavMenu").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementById("sideNavMenu").style.width = "0";
+}
